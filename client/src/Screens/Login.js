a90614acd41f2a1a63555f9cd076544d3705c563
Elijah Smith <mcditto99@gmail.com>
2023-04-04 19:21:14 -0400

    Login escaped the previous commit :(

--- client/src/Screens/Login.js
+++ client/src/Screens/Login.js
@@ -1,4 +1,4 @@
-import { StyleSheet, View, Image } from "react-native";
+import { StyleSheet, View, Image, Pressable, Text } from "react-native";
 
 export default function Login({ navigation }) {
 	return (
@@ -7,6 +7,19 @@ export default function Login({ navigation }) {
 				style={{ width: 150, height: 150 }}
 				source={require("../../assets/face.png")}
 			/>
+            <Text
+			<Pressable
+				style={styles.button}
+				onPress={() => navigation.navigate("Login")}
+			>
+				<Text style={styles.text}>Returning Friend</Text>
+			</Pressable>
+			<Pressable
+				style={styles.button}
+				onPress={() => navigation.navigate("Signup")}
+			>
+				<Text style={styles.text}>New Friend</Text>
+			</Pressable>
 		</View>
 	);
 }
@@ -17,5 +30,17 @@ const styles = StyleSheet.create({
 		backgroundColor: "#856A5d",
 		justifyContent: "center",
 		alignItems: "center",
+	},
+	button: {
+		backgroundColor: "#6C6F7D",
+		borderColor: "#000000",
+		borderWidth: 2,
+		marginTop: 60,
+		padding: 5,
+		borderRadius: 10,
+	},
+	text: {
+		color: "#000000",
+		fontSize: 24,
 	},
 });
